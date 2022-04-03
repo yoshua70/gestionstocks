140feeb0190a00709936323c87a4db22145feed0
Michee Allidjinou <allidjinoumichee@outlook.fr>
2022-04-03 18:31:06 +0000

    feat: fiche produit
    Lister les différents produits sur la page produit.
    Afficher les informations pour un produit donné sur la page
    ficheProduit.

--- base/views.py
+++ base/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+produits = [
+        {'libelle': 'Sneakers', 'designation': 'snk001', 'description': 'Des sneakers solides pour vous accompagner dans tous vos déplacements', 'statut': 'en stock'},
+        {'libelle': 'T-shirt Sportif', 'designation': 'tshirtsport', 'description': 'Du coton de qualité pour vos activités sportives', 'statut': 'en stock'},
+        {'libelle': 'Montre Tactique GX-0', 'designation': 'gx0', 'description': "Montre tactique, résistante à l'eau", 'statut': 'en stock'},
+    ]
+
 def home(request):
     return render(request, 'base/home.html')
 
@@ -17,9 +23,12 @@ def stocks(request):
     return HttpResponse("Stocks")
 
 def produit(request):
-    context = {}
+    context = { 'produits': produits }
     return render(request, 'base/produit.html', context)
 
 def ficheProduit(request, pk):
-    context = { 'idProduit': pk }
+    for i in produits: 
+        if i['designation'] == pk:
+            produit = i
+    context = { 'produit': produit }
     return render(request, 'base/ficheProduit.html', context)
