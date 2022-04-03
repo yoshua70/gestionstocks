9b093de4915ff44779bdac9d18a0325ba175ed0b
Michee Allidjinou <allidjinoumichee@outlook.fr>
2022-04-03 18:51:38 +0000

    chore: creation modeles
    Creer les modèles :
    - pour les produits
    - pour les stocks

--- base/models.py
+++ base/models.py
@@ -1,3 +1,26 @@
+from email.policy import default
 from django.db import models
 
 # Create your models here.
+
+class Produit(models.Model):
+    designation = models.CharField(max_length=50)
+    libelle = models.CharField(max_length=100)
+    description = models.TextField(null=True, max_length=500, blank=True)
+    statut = models.TextChoices('StatutStock', 'ENSTOCK RUPTURE')
+    updatedAt = models.DateTimeField(auto_now=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.designation
+
+class Stocks(models.Model):
+    designation = models.CharField(max_length=50)
+    quantité = models.FloatField()
+    unité = models.CharField(max_length=50)
+    type = models.TextChoices('TypeStock', 'PRODUIT MATIERES')
+    updatedAt = models.DateTimeField(auto_now=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.designation
