c41c853243261edbf84b4bdae5f5b47e432c9a1c
Michee Allidjinou <allidjinoumichee@outlook.fr>
2022-04-04 15:02:21 +0000

    fix: field statut
    Resoudre le probleme des choix pour le champ 'statut'.

--- base/models.py
+++ base/models.py
@@ -7,7 +7,8 @@ class Produit(models.Model):
     designation = models.CharField(max_length=50)
     libelle = models.CharField(max_length=100)
     description = models.TextField(null=True, max_length=500, blank=True)
-    statut = models.TextChoices('StatutStock', 'ENSTOCK RUPTURE')
+    StatutStock = models.TextChoices('StatutStock', 'ENSTOCK RUPTURE')
+    statut = models.CharField(blank=True, choices=StatutStock.choices, default='ENSTOCK', max_length=10)
     updatedAt = models.DateTimeField(auto_now=True)
     createdAt = models.DateTimeField(auto_now_add=True)
 
