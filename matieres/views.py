0b161f281b0d5dc51ef3a6d13ee3a85bb87d5397
ElenaPetrowsky <gkmichael01@gmail.com>
2022-04-05 23:52:04 +0200

    feat: création et modification de matière première fonctionnel

--- matieres/views.py
+++ matieres/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
 
 from matieres.models import MatierePremiere
+from .forms import MatierePremiereForm
 
 # Create your views here.
 
@@ -11,3 +12,23 @@ def matiere(request):
         'matierePremiere': listeMatiere
     }
     return render(request, 'matieres/matiere.html', context)
+
+def createMatiere(request):
+    form = MatierePremiereForm()
+    if request.method == 'POST':
+        form = MatierePremiereForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('matieres')
+
+    context = {'form': form, 'operation': "create"}
+    return render(request, 'matieres/matiere_form.html', context)
+
+def updateMatiere(request,pk):
+    instance = get_object_or_404(MatierePremiere, id=pk)
+    form = MatierePremiereForm(request.POST or None, instance=instance)
+    if form.is_valid():
+        form.save()
+        return redirect('matieres')
+    return render(request, 'matieres/matiere_form.html', {'form':form, 'operation':'update'})
+
