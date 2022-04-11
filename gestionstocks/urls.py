be69346351181a8bee2b4b69dc8ce5369067ac70
Michee Allidjinou <allidjinoumichee@outlook.fr>
2022-04-11 15:38:50 +0000

    feat: ajouter page accueil
    Ajouter la page d'accueil de l'application:

--- gestionstocks/urls.py
+++ gestionstocks/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
     path('produits/', include('produits.urls')),
     path('matieres/', include('matieres.urls')),
     path('commandes/', include('commandes.urls')),
