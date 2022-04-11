b9165577b5034286641bcd824ee89263f2c8a7b7
Michee Allidjinou <allidjinoumichee@outlook.fr>
2022-04-11 16:51:26 +0000

    chore: ajouer tailwind en local
    Creer dossier jstoolchain pour les utilitaires js
    Creer dossier static pour les fichiers statics de l'application

--- gestionstocks/settings.py
+++ gestionstocks/settings.py
@@ -130,6 +130,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    'static/'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
